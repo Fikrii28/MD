c4608efa348e30754067a536d9e951699a7a804a
Fikrii28 <107801844+Fikrii28@users.noreply.github.com>
2022-06-20 11:30:38 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1653,7 +1653,7 @@ break
             case 'cecan': case 'china': case 'thailand':  case 'kayes': case 'aeunicetjoaa': case 'natajadeh': {
                 m.reply(mess.wait)
                 let fetch = await global.api("zenz", "/randomasupan/" + command, {}, "apikey")
-                hisoka.sendFile(m.from, fetch, "vidio", m, { caption: "Random TikTok Asupan" }, { quoted: m })
+                hisoka.sendMessage(m.from, fetch, "", m, { caption: "Random TikTok Asupan" }, { quoted: m })
             }
            break
             case 'waifus': case 'nekos': case 'trap': case 'hentai': case 'ass': case 'paizuri': case 'blowjob': {
