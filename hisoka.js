621fb282f36f7398b6cbde325ab5bb668ae85c9f
Fikrii28 <107801844+Fikrii28@users.noreply.github.com>
2022-06-21 14:42:22 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1649,13 +1649,8 @@ break
                 result = anu[Math.floor(Math.random() * anu.length)]
                 hisoka.sendMessage(m.chat, { image: { url: result }, caption: '⭔ Media Url : '+result }, { quoted: m })
             }
-            break
-            case 'waifus': case 'nekos': case 'trap': case 'hentai': case 'ass': case 'paizuri': case 'blowjob': {
-                m.reply(mess.wait)
-                hisoka.sendMessage(m.chat, { image: { url: api('zenz', '/api/random/'+command, {}, 'apikey') }, caption: 'Generate Random ' + command }, { quoted: m })
-            }
             break 
-            case 'anime': case 'shinka': case 'sasuke': case 'sakura': case 'miku': case 'waifu': case 'husbu': case 'neko': case 'shinobu': case 'megumin': {
+            case 'anime': case 'waifu': case 'husbu': case 'neko': case 'shinobu': case 'megumin': case 'waifus': case 'nekos': case 'trap': case 'blowjob': {
                 m.reply(mess.wait)
                 hisoka.sendMessage(m.chat, { image: { url: api('zenz', '/api/random/'+command, {}, 'apikey') }, caption: 'Generate Random ' + command }, { quoted: m })
             }
